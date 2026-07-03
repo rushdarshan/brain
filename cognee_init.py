73753e6a562626dd1c2b20497f1585cf36b9feb2
rushdarshan <rushdarshan@gmail.com>
2026-07-03 20:12:56 +0530

    fix: make COGNEE_API_KEY optional

--- cognee_init.py
+++ cognee_init.py
@@ -7,7 +7,7 @@ os.environ.setdefault("CACHING", "true")
 
 import cognee
 
-COGNEE_API_KEY = os.environ["COGNEE_API_KEY"]
+COGNEE_API_KEY = os.environ.get("COGNEE_API_KEY", "")
 COGNEE_API_URL = os.environ.get("COGNEE_API_URL", "https://tenant-760a20b9-b8d7-42f0-9007-edf92f8817ff.aws.cognee.ai")
 
 
